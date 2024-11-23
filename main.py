22c89fa8d6b259c17b70c217bf95fc49accb14d3
Bezrukov47 <bezr47@gmail.com>
2024-11-23 21:57:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+my_list = len([42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5])
+index = 0
+print ('my_list'[0:3, 5])
+#while index < 11:
+    #index += 1
+    #for index in 'my_list':
+        #if index == 4:
+            #break
+        #continue
+        #if index == 6:
+            #break
+            #continue
+            #if index == 10:
+                #break
+                #continue
+                #print (my_list)
